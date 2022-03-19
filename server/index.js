4e99d2d25985606c704451cb8a91f8b2bff526de
eodinzov <eodinzov94@gmail.com>
2022-03-19 19:04:51 +0200

    First commit

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express();
+const PORT =  3000
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000'
+}));
+
+
+
+
+app.get('/', (req, res) => {
+    res.send(`Test`)
+
+})
+
+
+
+
+function runServer(){
+    try {
+        app.listen(PORT, () => console.log(`Server started on PORT : ${PORT}`))
+    }
+    catch (e) {
+        console.log(e)
+    }
+}
+
+runServer();
