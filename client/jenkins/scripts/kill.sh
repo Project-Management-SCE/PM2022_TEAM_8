bb77ddef8138667f4ce2ec2749c08b6e4deb0618
Evgeny <eodinzov94@gmail.com>
2022-03-22 19:25:46 +0200

    Jenkins test

--- client/jenkins/scripts/kill.sh
+++ client/jenkins/scripts/kill.sh
@@ -3,6 +3,7 @@
 echo 'The following command terminates the "npm start" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
 echo 'was executed.'
+cd client
 set -x
 ls
 kill $(cat .pidfile)
