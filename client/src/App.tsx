6cb06577c4f4c5eab67b5cf85690c7fd1bdce6d9
Ron Vayner <ronv.sce@gmail.com>
2022-03-29 21:29:42 +0300

    Revert "BSPM2022T8-135"
    This reverts commit 372cbab89ea5b20be2beb27f8f968a2bf0dc79d8.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,25 @@
-import React, { FC } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import Login from "./Routes/Login";
-import Register from "./Routes/Register";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
