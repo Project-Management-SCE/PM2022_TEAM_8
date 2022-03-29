f2c0c57a3ff137474dc2eabcd31b2cbf94d61d15
Ron Vayner <ronv.sce@gmail.com>
2022-03-29 20:47:52 +0300

    Revert " BSPM2022T8-16"
    This reverts commit 99112ba8d4779023a54cc3c3a68bb2394634ec82.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,26 @@
-import React, { FC } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import Login from "./Routes/Login";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import Register from "./Routes/Register";
-
-const App: FC = () => {
+function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
-function jsonServerProvider(arg0: string) {
-  throw new Error("Function not implemented.");
-}
