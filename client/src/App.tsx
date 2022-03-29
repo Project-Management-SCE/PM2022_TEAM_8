99112ba8d4779023a54cc3c3a68bb2394634ec82
Tal ohana <68163421+talohana55@users.noreply.github.com>
2022-03-29 17:25:13 +0300

    BSPM2022T8-16
    1.Create log in form + style + inputs&&button
    2.Add route to main App
    3.Add relevant packages to package-json file.
    4. fit index.html to bootstrap functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Login from "./Routes/Login";
 
-function App() {
+import Register from "./Routes/Register";
+
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
+function jsonServerProvider(arg0: string) {
+  throw new Error("Function not implemented.");
+}
