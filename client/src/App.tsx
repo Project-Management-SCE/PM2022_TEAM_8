6f2074f62a773becb7a50796db24fa11395d5f22
Tal ohana <68163421+talohana55@users.noreply.github.com>
2022-04-02 20:37:55 +0300

    BSPM2022T8-34
    1. Create Admin control page
    2. Add new route in main App
    3. Redirect to admin control page
    * Manual enter to admin page by insert directly '/admin' path - will be change

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useState, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Home from "./Routes/Home";
@@ -6,16 +6,28 @@ import MainNavigation from "./NavBar/MainNavigation";
 import Login from "./Routes/Login";
 import Register from "./Routes/Register";
 import UserProfile from "./Routes/UserProfile";
+import Admin from "./Routes/Admin";
 
 function App() {
+  const [adminON, setAdminON] = useState<boolean>(false);
+  var pathArray, adminUrl;
+  useEffect(() => {
+    pathArray = window.location.pathname.split("/");
+    adminUrl = pathArray[1];
+    if (adminUrl == "admin") {
+      setAdminON(true);
+    }
+  }, [pathArray]);
+
   return (
     <BrowserRouter>
-      <MainNavigation />
+      {!adminON && <MainNavigation />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/user_profile" element={<UserProfile />} />
+        <Route path="/admin" element={<Admin />} />
         <Route path="/" element={<Navigate replace to="/" />} />
       </Routes>
     </BrowserRouter>
