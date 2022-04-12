4f4e1c178bf9ae671cebb604773bf83648d48533
eodinzov <eodinzov94@gmail.com>
2022-04-12 22:47:37 +0300

    Jest Config Update

--- client/jest.config.js
+++ client/jest.config.js
@@ -2,11 +2,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  moduleNameMapper: {
-    "^src/(.*)": "<rootDir>/src/$1",
-  },
   transform: {
     "\\.(css|less|scss)$": "./jest/stub-transformer.js"
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
+  moduleNameMapper: {
+    "^~(.*)$": "<rootDir>/src/$1"
   },
   moduleDirectories: ["node_modules", "src"],
 };
