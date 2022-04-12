bb71259495b1d32131b0c0b33c9cb2166a4dd230
eodinzov <eodinzov94@gmail.com>
2022-04-12 23:03:27 +0300

    jest.config.js update

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,13 +1,18 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+const TEST_REGEX = "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|js?|tsx?|ts?)$";
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testRegex: TEST_REGEX,
   transform: {
-    "\\.(css|less|scss)$": "./jest/stub-transformer.js"
+    "\\.(css|less|scss)$": "./jest/stub-transformer.js",
+    "^.+\\.tsx?$": "babel-jest",
+    "^.+\\.tsx?$": "ts-jest",
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
+  testPathIgnorePatterns: ["<rootDir>/build/", "<rootDir>/node_modules/", "<rootDir>/dist/"],
   moduleNameMapper: {
     "^~(.*)$": "<rootDir>/src/$1"
   },
   moduleDirectories: ["node_modules", "src"],
 };
+
