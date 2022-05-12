52744784451be0a9ba046cc91d06233084c53e3e
eodinzov94 <eodinzov94@gmail.com>
2022-05-12 17:59:15 +0300

    jenkins deployment test

--- server/index.js
+++ server/index.js
@@ -8,6 +8,21 @@ const app = express();
 require('dotenv').config()
 const PORT = process.env.PORT || 3001
 const cors = require('cors')
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+        res.header(
+            "Access-Control-Allow-Methods",
+            "POST, PUT, PATCH, GET, DELETE"
+        )
+        return res.status(200).json({})
+    }
+    next()
+})
 app.use(express.json())
 app.use(cors())
 app.use("/api/user", authRoute)
