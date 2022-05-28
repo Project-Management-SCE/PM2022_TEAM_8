aae3daa8e56c4f0881e6e7712a74e87d8c9a923b
Ron Vayner <ronv.sce@gmail.com>
2022-05-28 21:07:55 +0300

    BSPM2022T8-175 BSPM2022T8-26 BSPM2022T8-174 BSPM2022T8-10 BSPM2022T8-173 BSPM2022T8-123
    1. Changed the wording on the "Sign up" banner for guests to reflect the site bonuses properly.
    2. Added integration tests for review addition, admin review viewing, reporting reviews and admin deleting reviews.

--- client/src/components/PopUp.tsx
+++ client/src/components/PopUp.tsx
@@ -9,17 +9,16 @@ export const PopUp = (props: { handleClose: any }) => {
       <div className="box">
         <button className="btn-close" onClick={props.handleClose}></button>
         <div className="popup-content">
-          <h2>Benefits of your W2W account</h2>
+          <h2>Benefits of your W2W (free!) account</h2>
           <div className="benefits">
-            <span>Personalized Recommendations</span>
-            <p>Discover shows you'll love.</p>
-            <span>Your Watchlist</span>
-            <p>
-              Track everything you want to watch and receive e-mail when movies
-              open in theaters.
-            </p>
+            <span>Keep track of your currently watched content in your watchlist!</span>
+            <p>Discover content you'll love, and keep track of it!</p>
             <span>Your Reviews</span>
-            <p>Rate and remember everything you've seen.</p>
+            <p>
+              Write what you thought about the content, for everyone to see!
+            </p>
+            <span>Full access</span>
+            <p>Unlimited access the site, browse all the series and movies, and look at all the expanded info about our content!</p>
           </div>
           <NavLink to="/register" className="linkStyle">
             <button>Create Account</button>
